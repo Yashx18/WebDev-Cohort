d7f084e86e9656d65f7191e04131ee7d4d2f4941
Ken <yash20004x17@gmail.com>
2025-03-24 12:55:23 +0530

    files added

--- script.js
+++ script.js
@@ -21,9 +21,9 @@ function users(arr) {
     if (arr[i].age > 18 && arr[i].gender == "male") {
       console.log(arr[i].firstName);
     }
-  } 
+  }
 
-//   let values = arr.filter()
+  //   let values = arr.filter()
 }
 
 const user01 = [
@@ -54,6 +54,66 @@ const user01 = [
   },
 ];
 
-users(user01);
+// users(user01);
 
 // console.log(user01);
+function add(n) {
+  let num = 1;
+  for (let i = 0; i <= n; i++) {
+    num = num + i;
+  }
+  return num;
+}
+
+const ans = add(10);
+// console.log(ans);
+
+const fs = require("fs");
+const path1 = fs.readFileSync("a.txt", "utf-8");
+const path2 = fs.readFileSync("b.txt", "utf-8");
+
+// console.log(path1);
+// console.log(path2);
+
+// Making a class in JS by myself
+
+class rectangle {
+  constructor(height, width, color) {
+    this.height = height;
+    this.width = width;
+    this.color = color;
+  }
+
+  area() {
+    return this.height * this.width;
+  }
+
+  paint() {
+    return `Color of the Rectangle is ${this.color}`;
+  }
+}
+const rect = new rectangle(2, 4, "red");
+const rectArea = rect.area();
+const rectColor = rect.paint();
+// console.log(rectArea);
+// console.log(rectColor);
+
+function random() {
+
+}
+
+//let p = new Promise(random); // it is supposed to return something eventually.
+//console.log(p);
+//
+function callfn(resolve){
+	console.log('callfn Called');
+	resolve();
+}
+let p = new Promise(callfn)
+
+function callback(){
+	console.log('Resolve function called')
+	console.log('Promise is fulfilled')
+}
+
+p.then(callback)
