55a91d55d8d506f5530ff192c7ed07e38813d61c
ken <yash20004x17@gmail.com>
2025-04-06 13:35:30 +0000

    Working on express server from week 5.2

--- index.js
+++ index.js
@@ -1,35 +1,56 @@
+const { timeStamp } = require("console");
 const express = require("express");
 const app = express();
 app.use(express.json());
 
-app.get("/add/:num1/:num2", function (req, res) {
-  const a = parseInt(req.params.num1);
-  const b = parseInt(req.params.num2);
+function middlewareFn(req, res, next) {
+  console.log(`Method used is ${req.method}`);
+  console.log(`URL used is ${req.hostname}`);
+  const time = Date.now();
+  console.log(time);
+
+  next();
+}
+
+app.post("/add", function (req, res) {
+  const a = parseInt(req.body.a);
+  const b = parseInt(req.body.b);
   res.json({
     msg: a + b,
   });
 });
-app.get("/multiply/:num1/:num2", function (req, res) {
-  const a = parseInt(req.params.num1);
-  const b = parseInt(req.params.num2);
-  res.json({
-    msg: a * b,
-  });
-});
-app.get("/divide/:num1/:num2", function (req, res) {
-  const a = parseInt(req.params.num1);
-  const b = parseInt(req.params.num2);
-  res.json({
-    msg: a / b,
-  });
-});
-app.get("/subract/:num1/:num2", function (req, res) {
-  const a = parseInt(req.params.num1);
-  const b = parseInt(req.params.num2);
-  res.json({
-    msg: a - b,
-  });
-});
+
+app.use(middlewareFn);
+
+// app.get("/add/:num1/:num2", middlewareFn, function (req, res) {
+//   const a = parseInt(req.params.num1);
+//   const b = parseInt(req.params.num2);
+//   res.json({
+//     msg: a + b,
+//   });
+// });
+
+// app.get("/multiply/:num1/:num2", function (req, res) {
+//   const a = parseInt(req.params.num1);
+//   const b = parseInt(req.params.num2);
+//   res.json({
+//     msg: a * b,
+//   });
+// });
+// app.get("/divide/:num1/:num2", function (req, res) {
+//   const a = parseInt(req.params.num1);
+//   const b = parseInt(req.params.num2);
+//   res.json({
+//     msg: a / b,
+//   });
+// });
+// app.get("/subract/:num1/:num2", function (req, res) {
+//   const a = parseInt(req.params.num1);
+//   const b = parseInt(req.params.num2);
+//   res.json({
+//     msg: a - b,
+//   });
+// });
 
 app.listen(3000, () => {
   console.log(`Server running on 'http://localhost:3000/add'`);
